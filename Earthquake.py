29b8c6eaa703c06fa9a53af372e057186b7fd634
Timothy Sandoval <tsandoval4343@gmail.com>
2014-06-09 13:07:15 -0400

    committing again

--- Earthquake.py
+++ Earthquake.py
@@ -1,1 +1,17 @@
-print "Hello world" 
+# D.R.Y. = don't repeat yourself 
+# W.E.T. Write Everything Twice 
+def greeting(): 
+    name = raw_input("What is your name? ")
+    print "Hello, " + name
+
+def hello(name):
+    print "Hello, " + name 
+def add(a, b):
+    return a + b
+    
+hello("Tim")
+
+print add(2, 3) + add(4, 5)
+
+
+
